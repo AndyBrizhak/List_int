859931c94d9386fb7cbbd2fc819a0f793d2b9ee5
Andrey Brizhak <andrewbrizhak@gmail.com>
2018-01-30 12:18:04 +0300

    Добавьте файлы проекта.

---
+++ ListT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
