9b8af2db732c8dbd01374e9f8306a4bf3e851946
Andrey Brizhak <andrewbrizhak@gmail.com>
2018-01-30 14:53:24 +0300

    +array list working

--- ListT/Program.cs
+++ ListT/Program.cs
@@ -15,12 +15,53 @@ namespace List_T
     {
         static void Main(string[] args)
         {
+            //требуется подсчитать, сколько раз каждый элемент встречается в данной коллекции для целых чисел using System.Collections;
             ArrayList list = new ArrayList();
-            Random rnd = new Random     ;
-            list.Add( 1 );
-            list.Add(1);
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int c = rnd.Next(1, 4);
+                list.Add(c);
+                //Console.WriteLine(c);
+            }
+
+            foreach (object element in list)
+                Console.WriteLine(element);
+            int count1 = 0;
+            int count2 = 0;
+            int count3 = 0;
+            int count4 = 0;
+
+            foreach (object element in list)
+                if (1.Equals(element))
+                {
+                    count1++;
+                }
+
+                else if (2.Equals(element))
+                {
+                    count2++;
+                }
+
+                else if (3.Equals(element))
+                {
+                    count3++;
+                }
+
+                else if (4.Equals(element))
+                {
+                    count4++;
+                }
+            Console.WriteLine("1 " + " - " + count1 + " раз");
+            Console.WriteLine("2 " + " - " + count2 + " раз");
+            Console.WriteLine("3 " + " - " + count3 + " раз");
+            Console.WriteLine("4 " + " - " + count4 + " раз");
+            Console.ReadKey();
 
             List<int> a = new List<int> { 1, 2, 4, 3, 5, 4, 4, 2, 6 };
+            foreach (object element in a)
+                Console.WriteLine(element);
+            Console.ReadKey();
             Console.WriteLine("Подсчитать, сколько раз каждый элемент встречается вданной коллекции используя Linq");
             foreach (int val in a.Distinct())
             {
