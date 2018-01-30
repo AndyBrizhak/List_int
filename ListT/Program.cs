f959c4bee4629e1d64faf234aa871fee29baca73
Andrey Brizhak <andrewbrizhak@gmail.com>
2018-01-30 13:00:05 +0300

    use Link work

--- ListT/Program.cs
+++ ListT/Program.cs
@@ -4,12 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ListT
+//Брижак Андрей Домашняя работа 4 C# продвинутый
+//Дана коллекция List<T>, требуется подсчитать, сколько раз каждый элемент встречается в
+//данной коллекции.
+
+namespace List_T
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<int> a = new List<int> { 1, 2, 4, 3, 5, 4, 4, 2, 6 };
+
+
+
+            Console.WriteLine("Подсчитать, сколько раз каждый элемент встречается вданной коллекции используя Linq");
+            foreach (int val in a.Distinct())
+            {
+                Console.WriteLine(val + " - " + a.Where(x => x == val).Count() + " раз");
+                
+            }
+            Console.ReadKey();
+                     
         }
     }
 }
