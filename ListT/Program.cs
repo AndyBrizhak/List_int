422ee3ae5e78a791cdbfbb539b4ae6df86ad373c
AndyBrizhak <andrewbrizhak@gmail.com>
2018-02-06 17:47:49 +0300

    add dictionary method

--- ListT/Program.cs
+++ ListT/Program.cs
@@ -20,8 +20,8 @@ namespace List_T
             Random rnd = new Random();
             for (int i = 0; i < 10; i++)
             {
-                int c = rnd.Next(1, 4);
-                list.Add(c);
+                int z = rnd.Next(1, 4);
+                list.Add(z);
                 //Console.WriteLine(c);
             }
 
@@ -33,6 +33,7 @@ namespace List_T
             int count4 = 0;
 
             foreach (object element in list)
+            {
                 if (1.Equals(element))
                 {
                     count1++;
@@ -52,15 +53,50 @@ namespace List_T
                 {
                     count4++;
                 }
+            }
+
             Console.WriteLine("1 " + " - " + count1 + " раз");
             Console.WriteLine("2 " + " - " + count2 + " раз");
             Console.WriteLine("3 " + " - " + count3 + " раз");
             Console.WriteLine("4 " + " - " + count4 + " раз");
             Console.ReadKey();
+            //Дана коллекция List<T>, требуется подсчитать, сколько раз каждый элемент встречается в
+            //данной коллекции с использованием словаря
+            Dictionary<string, int> dict = new Dictionary<string, int>()
+            {
+            {"four",4 },
+            {"two",2 },
+            { "one",1 },
+            {"three",3 },
+            };
+
+            var d = dict.OrderBy(delegate (KeyValuePair<string, int> pair) { return pair.Value; });
+            foreach (var pair in d)
+            {
+                Console.WriteLine("{0} - {1}", pair.Key, pair.Value);
+            }
+            Console.WriteLine("");
+
+            var c = dict.OrderBy(pair => pair.Value);
+            foreach (var pair in c)
+            {
+                Console.WriteLine("{0} - {1}", pair.Key, pair.Value);
+            }
+            Console.WriteLine("");
+            //Func<KeyValuePair<string, int>, int> predicate = Order;
+            //var e = dict.OrderBy(predicate);
+            //foreach (var pair in e)
+            //{
+            //    Console.WriteLine("{0} - {1}", pair.Key, pair.Value);
+            //}
+
+
+
+
             //            Дана коллекция List<T>, требуется подсчитать, сколько раз каждый элемент встречается в данной коллекции
             //            б) *для обобщенной коллекции;
             //            в)**используя Linq.
-                        List<int> a = new List<int> { 1, 2, 4, 3, 5, 4, 4, 2, 6 };
+            List<int> a = new List<int> { 1, 2, 4, 3, 5, 4, 4, 2, 6 };
             foreach (object element in a)
                 Console.WriteLine(element);
             Console.ReadKey();
@@ -71,7 +107,9 @@ namespace List_T
                 
             }
             Console.ReadKey();
-                     
+
+           
+
         }
     }
 }
